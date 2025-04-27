bf42760212a4e4463f6d51df02b38989e8fb5649
angel zambrano <angeldzambrano99@gmail.com>
2025-04-26 23:53:56 -0700

    added base project

--- savyFinder/src/App.tsx
+++ savyFinder/src/App.tsx
@@ -1,6 +1,7 @@
 import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Dashboard from './Dashboard'
 
 
 
@@ -10,16 +11,7 @@ import './index.css'
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold">
-          Welcome to my App
-        </h1>
-        <p className="mt-2 text-gray-600">
-          Built with Vite + React + TailwindCSS 🚀
-        </p>
-      </div>
-    </div>
+    Dashboard()
   )
 }
 
